f1fe70e90ae1cb01eabe2a052a3554a791a18db0
Hrishikesh Deshmukh <59553879+hrishikeshrd@users.noreply.github.com>
2020-10-03 22:17:56 +0530

    Update Donation.dart

--- lib/Donation/Donation.dart
+++ lib/Donation/Donation.dart
@@ -1,13 +1,1 @@
-import 'package:flutter/material.dart';
 
-class Donation extends StatefulWidget {
-  @override
-  _DonationState createState() => _DonationState();
-}
-
-class _DonationState extends State<Donation> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-  }
-}
